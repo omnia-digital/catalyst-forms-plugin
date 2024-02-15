521e21611b45588012da83292827a517d1330a42
Josh Torres <josht@omniadigital.io>
2024-02-15 10:27:11 -0800

    - fix namespace error

--- src/Policies/FormSubmissionPolicy.php
+++ src/Policies/FormSubmissionPolicy.php
@@ -3,7 +3,6 @@
 namespace OmniaDigital\CatalystForms\Policies;
 
 use App\Models\User;
-use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use OmniaDigital\CatalystForms\Models\FormSubmission;
 
